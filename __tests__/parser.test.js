a123d094f5c1b38c595c022b79cbb36b6c903abd
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-09-14 16:20:19 -0700

    add parser function and test (in progress)

---
+++ __tests__/parser.test.js
@@ -0,0 +1,24 @@
+const parser = require('../lib/parser.js');
+const request = require('../lib/request.js');
+
+describe('tests the book parse function', () => {
+  it('should take the document html and return an array of all the books and their details', async() => {
+    
+    // get the document
+    const document = await request();
+
+    // get the li/the books from the document
+    const bookDetails = await parser(document);
+
+    // console.log('BOOK DETAILS', bookDetails);
+
+    // expect to equal an array containing a random book and details from page.
+    expect(bookDetails).toBeTruthy();
+
+    // expect(bookDetails).toEqual(expect.arrayContaining([
+    //   { title: 'A Light in the Attic', cover_image: 'catalogue/a-light-in-the-attic_1000/index.html', rating: 'Three', price: '£51.77', in_stock: true },
+    //   { title: 'Sapiens: A Brief History', cover_image: 'catalogue/sapiens-a-brief-history-of-humankind_996/index.html', rating: 'Five', price: '£54.23', in_stock: true }
+    // ]));
+
+  });
+});
