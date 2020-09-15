f137e0d2f8c85095e4abe20b5df78de2ab76b750
katiePDX <61799774+katiepdx@users.noreply.github.com>
2020-09-15 14:43:42 -0700

    update request function for pages and tests

--- __tests__/request.test.js
+++ __tests__/request.test.js
@@ -3,7 +3,7 @@ const request = require('../lib/request.js');
 
 describe('Book model request function', () => {
   it('tests that the request function makes a request to the books to scrape', async() => {
-    const document = await request();
+    const document = await request(2);
     
     // find an element on the page and set the expected for that content 
     expect(document.querySelector('.action>h1').textContent).toEqual('All products');
